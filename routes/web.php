e9fdbe09acb497f161275e18cef93978589eb9c5
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-06-06 16:26:11 +0200

    moved controller and created route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\Typecontroller;
+use App\Http\Controllers\Admin\Technologycontroller;
+
 
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +28,8 @@ Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('projects', ProjectController::class)->parameters(['projects'=>'project:slug']);
     Route::resource('types', TypeController::class)->parameters(['types' => 'type:slug']);
+    Route::resource('technologies', TechnologyController::class)->parameters(['technologies' => 'technology:slug']);
+
 });
 
 
